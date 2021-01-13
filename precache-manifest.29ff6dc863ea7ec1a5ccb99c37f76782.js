e575c016ee2c48a24eb3184c19344eba0c1ffda6
dhruvinoptyfi <dhruvin@opty.fi>
2021-01-13 01:57:56 -0500

    Updates

---
+++ precache-manifest.29ff6dc863ea7ec1a5ccb99c37f76782.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75a2890d005dd8126eaf19ec27425cd8",
+    "url": "/simple-node-js-react-npm-app/index.html"
+  },
+  {
+    "revision": "3489da9eff4a8276259b",
+    "url": "/simple-node-js-react-npm-app/static/css/main.4bf56663.chunk.css"
+  },
+  {
+    "revision": "df30f18c3a293f05b50b",
+    "url": "/simple-node-js-react-npm-app/static/js/2.117eaf8d.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/simple-node-js-react-npm-app/static/js/2.117eaf8d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3489da9eff4a8276259b",
+    "url": "/simple-node-js-react-npm-app/static/js/main.dae316a4.chunk.js"
+  },
+  {
+    "revision": "9a9bef2bcf10e92cc082",
+    "url": "/simple-node-js-react-npm-app/static/js/runtime-main.b46a5e76.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/simple-node-js-react-npm-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
